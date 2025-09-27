03cc797273ce48d87a7461d8d19f1ee10041e854
streaktv8@gmail.com <streaktv8@gmail.com>
2025-09-27 10:57:24 +0100

    chnagdd json

--- src/components/RequireSuperAdmin.tsx
+++ src/components/RequireSuperAdmin.tsx
@@ -2,10 +2,10 @@
 import { Navigate } from 'react-router-dom'
 import { useAuth } from '../hooks/useAuth'
 
-export default function RequireSuperAdmin({ children }: { children: JSX.Element }) {
+export default function RequireSuperAdmin({ children }: { children: any }) {
   const { user, loading } = useAuth()
 
-  if (loading) return null // or show a spinner
+  if (loading) return null // or show a spinnerx
 
   if (!user || user.role !== 'SUPERADMIN') {
     return <Navigate to="/vendors" replace />
